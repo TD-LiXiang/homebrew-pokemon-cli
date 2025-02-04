f9ec8160d0ead02adc100a458cad63e12b3e88ee
Xiang Li <xiang.li@talkdesk.com>
2025-02-05 02:01:27 +0800

    update new version 0.1.1

--- Formula/pokemon-cli.rb
+++ Formula/pokemon-cli.rb
@@ -1,9 +1,9 @@
 class PokemonCli < Formula
   desc "Command-line interface for Pokémon"
   homepage "https://github.com/TD-LiXiang/pokemon-cli"
-  url "https://github.com/TD-LiXiang/pokemon-cli/releases/download/0.1.0/pokemon"
-  version "0.1.0"
-  sha256 "c14b934d1217c65078cc42349a9f576bc33284c87cb865db66d524eb67574ec8"
+  url "https://github.com/TD-LiXiang/pokemon-cli/releases/download/0.1.1/pokemon"
+  version "0.1.1"
+  sha256 "5aa47f558ea79185c10396a46cf398ab674d190aeb439361f21aba56235ed57f"
 
   def install
     bin.install "pokemon"
